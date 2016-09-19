9026e4220a03253b7803ed94b711ca89f6cd39db
Fred Emmott <github@fredemmott.co.uk>
2016-09-18 17:07:14 -0700

    add HasUriPattern, UriPatternDerivatives

--- examples/UriPatternsExample.php
+++ examples/UriPatternsExample.php
@@ -18,19 +18,19 @@ namespace Facebook\HackRouter\Examples\UrlPatternsExample;
 require_once('../vendor/autoload.php');
 
 use Facebook\HackRouter\BaseRouter;
+use Facebook\HackRouter\HasUriPattern;
 use Facebook\HackRouter\HttpMethod;
 use Facebook\HackRouter\UriBuilder;
 use Facebook\HackRouter\UriPattern;
+use Facebook\HackRouter\UriPatternDerivatives;
 use Facebook\HackRouter\UriParameters;
 
 <<__ConsistentConstruct>>
-abstract class WebController {
-  abstract public function getResponse(): string;
-  abstract public static function getUriPattern(): UriPattern;
+abstract class WebController implements HasUriPattern {
+  // Provides getUriBuilder(), getFastRoutePattern()
+  use UriPatternDerivatives;
 
-  final public static function getUriBuilder(): UriBuilder {
-    return (new UriBuilder(static::getUriPattern()->getParts()));
-  }
+  abstract public function getResponse(): string;
 
   private UriParameters $uriParameters;
   final protected function getUriParameters(): UriParameters {
@@ -84,8 +84,8 @@ final class UriPatternsExample extends BaseRouter<TResponder> {
   ): ImmMap<HttpMethod, ImmMap<string, TResponder>> {
     $urls_to_controllers = Map { };
     foreach (self::getControllers() as $controller) {
-      $pattern = $controller::getUriPattern();
-      $urls_to_controllers[$pattern->getFastRouteFragment()] = $controller;
+      $pattern = $controller::getFastRoutePattern();
+      $urls_to_controllers[$pattern] = $controller;
     }
     return ImmMap {
       HttpMethod::GET => $urls_to_controllers->immutable(),
