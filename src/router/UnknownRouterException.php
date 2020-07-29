98054ee0f283485938b1c739a5f596af7e326b4e
jjergus <jjergus@fb.com>
2020-07-29 10:23:41 -0700

    array -> varray typehint

--- src/router/UnknownRouterException.php
+++ src/router/UnknownRouterException.php
@@ -11,13 +11,13 @@
 namespace Facebook\HackRouter;
 
 class UnknownRouterException extends InternalServerErrorException {
-  public function __construct(private array<mixed> $fastRouteData) {
+  public function __construct(private varray<mixed> $fastRouteData) {
     parent::__construct(
       'Unknown FastRoute response: '.\var_export($fastRouteData, true),
     );
   }
 
-  public function getFastRouteData(): array<mixed> {
+  public function getFastRouteData(): varray<mixed> {
     return $this->fastRouteData;
   }
 }
